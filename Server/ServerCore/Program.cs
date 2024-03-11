0984ad3d2c4244ecea31b0d3382a4f381f64c057
Alisser <gnlrjswjd@naver.com>
2024-03-11 15:31:48 +0900

    etc update

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -7,64 +7,57 @@ using System.Threading.Tasks;
 
 namespace DummyClient
 {
-    // 메모리 배리어
-    // 1. 코드 재배치 억제
-    // 2. 가시성 - 전광판에 커믿해서 남들도 볼수있게
+    class Lock
+    {
+        AutoResetEvent _available = new AutoResetEvent(true); // 첫 인자로 게이트를 열고 시작할지 아닐지 선택 (그리도 자동으로 문 닫아줌)
 
-    // 1) Full Memory Barrier(Thread.MemoryBarrier) - store/load 둘다 막음 | 스토어는 변수에 값 매기는거 로드는 변수의 값을 끄집어내느거
-    // 2) Store Memory Barrier
-    // 3) Load Memory Barrier
+        public void Acquire() // 획득
+        {
+            _available.WaitOne(); // 입장시도
+            //_available.Reset(); // bool = false, 위 코드에 포함되어 있기에 생략 가능
+
+
+        }
+
+        public void Release() // 내려놓기
+        {
+            _available.Set(); // 다시 true로 바꿔준다
+        }
+    }
     class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int _num = 0;
+        static Lock _lock = new Lock();
 
         static void Thread_1()
         {
-            y = 1; // store y
-            // --------------------------
-            Thread.MemoryBarrier();// 풀 메모리 배리어 장벽을세워 못넘어가게 하는것같음
-
-
-
-            r1 = x; // load x
+            for (int i = 0; i < 10000; i++)
+            {
+                _lock.Acquire();// Acquire 습득하다
+                _num++;
+                _lock.Release();// Release 풀어주다
+            }
         }
         static void Thread_2()
         {
-            x = 1; // store x
-            // --------------------------
-            Thread.MemoryBarrier();
-
-
-            r2 = y; // load y
+            for (int i = 0; i < 10000; i++)
+            {
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                x = y = r1 = r2 = 0;
+            Task.WaitAll(t1, t2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-
-            }
-            Console.WriteLine($"{count}번만에 빠져나옴");
-            //탈출이 되는이유 - 하드웨어는 명령이 서로 의존성이 없다 판단하면 스스로 순서를 바꾸는 최적화를 진행하기 떄문에 이런 일이 발생할수 있다
-
-
+            Console.WriteLine( _num);
         }
     }
 }
